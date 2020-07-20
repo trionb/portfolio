218cd4d890af19b47ad87be8112b70a4751c5115
trion bailey <trionbailey@gmail.com>
2020-07-20 11:19:29 -0400

    Updates

---
+++ precache-manifest.78ddd88f02e09da3b65ad0007b8aec4f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f4ea11b9888bf09b981b5d6ad5886a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8f4d7bf1e0ee2acd9a30",
+    "url": "/portfolio/static/css/main.2b7982ef.chunk.css"
+  },
+  {
+    "revision": "61ac743d4bd40f8f3bd5",
+    "url": "/portfolio/static/js/2.e97ed08f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e97ed08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4d7bf1e0ee2acd9a30",
+    "url": "/portfolio/static/js/main.3baca7eb.chunk.js"
+  },
+  {
+    "revision": "0ccca4841728e8a7aba2",
+    "url": "/portfolio/static/js/runtime-main.92d286a2.js"
+  },
+  {
+    "revision": "6d0a3eb99618b6ca58dc98e63df2e829",
+    "url": "/portfolio/static/media/fyre.6d0a3eb9.png"
+  },
+  {
+    "revision": "82a27040077e1ab8fec03bf850579f89",
+    "url": "/portfolio/static/media/me.82a27040.jpeg"
+  },
+  {
+    "revision": "4461c86d4b8d1cd3779156c7d5985841",
+    "url": "/portfolio/static/media/modern.4461c86d.png"
+  },
+  {
+    "revision": "1d6d7d3e36887af743a76b323b524e88",
+    "url": "/portfolio/static/media/note-taker.1d6d7d3e.png"
+  },
+  {
+    "revision": "615300605ab41e482eb114a491c9e6ac",
+    "url": "/portfolio/static/media/passwordgenerator.61530060.png"
+  },
+  {
+    "revision": "4e7a8112a1b1d80d8c9c7e1d01c4c9fa",
+    "url": "/portfolio/static/media/solveStaking.4e7a8112.png"
+  },
+  {
+    "revision": "72403ae07a457b9e0dc364c6fac34a08",
+    "url": "/portfolio/static/media/soundtrackmovie.72403ae0.png"
+  },
+  {
+    "revision": "7db2fffad57de7709442ac0e285484c8",
+    "url": "/portfolio/static/media/sushi.7db2fffa.png"
+  }
+]);
